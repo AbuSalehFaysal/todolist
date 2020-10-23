dbdddf9d9ceb6d312fa4fc2b3e12c1550d2f221f
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2020-10-24 00:04:27 +0600

    Mark completed a task and delete a task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+// select the list and add line through effect 
+$("li").click(function(){
+    $(this).toggleClass("completed");
+});
 
+//delet a task 
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
