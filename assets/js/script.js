0c5c4fefe7c5b9fd0c7f85b175cb693da2463ab7
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2020-10-24 02:22:38 +0600

    add new todos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 // select the list and add line through effect 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
 //delet a task 
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
+});
+//add new todo list
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var todo = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span> " + todo + " </li>");
+    }
 });
