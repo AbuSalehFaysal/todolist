47bbc7fcb436e475520729be4f565a5488b8cb9f
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2020-10-26 20:49:42 +0600

    Frontend update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,29 @@
-// select the list and add line through effect 
-$("ul").on("click", "li", function(){
-    $(this).toggleClass("completed");
+// select the list and add line through effect
+$("ul").on("click", "li", function () {
+  $(this).toggleClass("completed");
 });
 
-//delet a task 
-$("ul").on("click", "span", function(event){
-    $(this).parent().fadeOut(500, function(){
-        $(this).remove();
+//delet a task
+$("ul").on("click", "span", function (event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
+      $(this).remove();
     });
-    event.stopPropagation();
+  event.stopPropagation();
 });
 //add new todo list
-$("input[type='text']").keypress(function(event){
-    if(event.which === 13){
-        var todo = $(this).val();
-        $(this).val("");
-        $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todo + " </li>");
-    }
+$("input[type='text']").keypress(function (event) {
+  if (event.which === 13) {
+    var todo = $(this).val();
+    $(this).val("");
+    $("ul").append(
+      "<li><span><i class='fas fa-trash-alt'></i></span> " + todo + " </li>"
+    );
+  }
 });
 
 //plus icon toggle
-$(".fa-plus-circle").click(function(){
-    $("input[type='text']").fadeToggle();
+$(".fa-plus-circle").click(function () {
+  $("input[type='text']").fadeToggle(10);
 });
